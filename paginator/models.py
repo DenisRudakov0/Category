bbb0e617dc4177a19c63fe790af50586c9bc4110
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-24 16:59:11 +0300

    makemigration_two

--- paginator/models.py
+++ paginator/models.py
@@ -4,6 +4,8 @@ class Product(models.Model):
     title = models.CharField('Название', max_length = 50)
     anons = models.CharField('Ссылка на фото', max_length = 50)
     full_text = models.TextField('Описание товара')
+    cena = models.CharField('Цена', max_length = 10)
+    cena_action = models.CharField('Цена со скидкой', max_length = 10)
     date = models.DateTimeField('Дата публикации')
     date_start = models.DateTimeField('Дата начала акции')
     date_end = models.DateTimeField('Дата окончания акции')
