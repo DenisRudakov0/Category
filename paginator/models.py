77e3fb5aef736c3b7538f561278987e922aadd6d
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-24 16:32:53 +0300

    makemigration_one

--- paginator/models.py
+++ paginator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField('Название', max_length = 50)
+    anons = models.CharField('Ссылка на фото', max_length = 50)
+    full_text = models.TextField('Описание товара')
+    date = models.DateTimeField('Дата публикации')
+    date_start = models.DateTimeField('Дата начала акции')
+    date_end = models.DateTimeField('Дата окончания акции')
+
+    def __str__(self):
+        return f'Новость: {self.title}'
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-date']
