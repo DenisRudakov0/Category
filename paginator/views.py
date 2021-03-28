c5199099e2f6c051aa888428c1544f59b4613326
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-28 09:05:03 +0300

    Add_paginator

--- paginator/views.py
+++ paginator/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 def news_home(request):
     news = Product.objects.all()
-    return render(request, 'paginator/news_home.html', {'news': news})
+    paginator = Paginator(news, 3)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+    }
+
+    return render(request, 'paginator/news_home.html', {'page_object': page, 'is_paginated': is_paginated, 'next_url': next_url, 'prev_url': prev_url})
